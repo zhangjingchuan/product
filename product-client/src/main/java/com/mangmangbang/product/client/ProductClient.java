7e0a56a39336ff19c049dcbf11097d8a379c21f9
zhangjingchuan <64677664@qq.com>
2020-01-12 20:14:33 +0800

    fegin-hystrix

--- product-client/src/main/java/com/mangmangbang/product/client/ProductClient.java
+++ product-client/src/main/java/com/mangmangbang/product/client/ProductClient.java
@@ -3,12 +3,13 @@ package com.mangmangbang.product.client;
 import com.mangmangbang.common.dto.DecreaseStockInput;
 import com.mangmangbang.common.dto.ProductInfoOutput;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.List;
 
-@FeignClient(name = "PRODUCT")
+@FeignClient(name = "PRODUCT",fallback = ProductClient.ProductClientFallback.class)
 public interface ProductClient {
 
     /**
@@ -25,4 +26,18 @@ public interface ProductClient {
      */
     @PostMapping("/product/decreaseStock")
     public void decreaseStock(@RequestBody List<DecreaseStockInput> decreaseStockInputList);
+
+    @Component
+    static class ProductClientFallback implements ProductClient{
+
+        @Override
+        public List<ProductInfoOutput> listForOrder(List<String> productIdList) {
+            return null;
+        }
+
+        @Override
+        public void decreaseStock(List<DecreaseStockInput> decreaseStockInputList) {
+
+        }
+    }
 }
